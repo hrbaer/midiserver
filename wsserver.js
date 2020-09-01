0e1c4efa6130ab769ca7762d646dbaeb532bfc00
H. R. Baer <hbaer@ethz.ch>
2020-09-01 22:35:49 +0200

    Handling of HTTPS

--- wsserver.js
+++ wsserver.js
@@ -2,8 +2,33 @@ module.exports = function (options, callback) {
   
   let port = options.port || 8080
 
-  var WebSocketServer = require('ws').Server,
-      wss = new WebSocketServer({ port });
+  const fs = require('fs');
+  const https = require('https');
+  const WebSocket = require('ws')
+  const path = require('path')
+
+  // Define path to certificates
+  const home = require('os').homedir()
+  const dir = path.join(home, 'certificates')
+
+/*
+
+  When using https, you might need a (self-signed) certifcate.
+  Enter commands from the conosle similar to this:
+
+  openssl genrsa -out key.pem
+  openssl req -new -key key.pem -out csr.pe
+  openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+  rm csr.pem
+
+*/
+
+  const server = https.createServer({
+    cert: fs.readFileSync(path.join(dir, 'cert.pem')),
+    key: fs.readFileSync(path.join(dir, 'key.pem'))
+  });
+
+  const wss = new WebSocket.Server({ server, port });
 
   wss.on('connection', (client, req) => {
 
